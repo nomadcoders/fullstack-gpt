388cf3e3e5fbcc427643f47170aa9a2c4ef056dc
니꼬 <nico@nomadcoders.co>
2023-11-08 17:54:26 +0900

    5.4 Chat Messages

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,3 +1,38 @@
+import time
 import streamlit as st
 
+st.set_page_config(
+    page_title="DocumentGPT",
+    page_icon="📃",
+)
+
 st.title("DocumentGPT")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.write(message)
+    if save:
+        st.session_state["messages"].append({"message": message, "role": role})
+
+
+for message in st.session_state["messages"]:
+    send_message(
+        message["message"],
+        message["role"],
+        save=False,
+    )
+
+
+message = st.chat_input("Send a message to the ai ")
+
+if message:
+    send_message(message, "human")
+    time.sleep(2)
+    send_message(f"You said: {message}", "ai")
+
+    with st.sidebar:
+        st.write(st.session_state)
