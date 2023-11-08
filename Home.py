0838680a88d05d2df1552cfbd6065cc09de04bee
니꼬 <nico@nomadcoders.co>
2023-11-08 16:06:35 +0900

    5.1 Magic

--- Home.py
+++ Home.py
@@ -1,11 +1,10 @@
 import streamlit as st
 
-st.title("Hello world!")
 
-st.subheader("Welcome to Streamlit!")
-
-st.markdown(
-    """
-    #### I love it!
-"""
+st.selectbox(
+    "Choose your model",
+    (
+        "GPT-3",
+        "GPT-4",
+    ),
 )
