72e05173f1ee8b15c0c9173c8e55ccf80c4295a6
니꼬 <nico@nomadcoders.co>
2023-11-08 16:28:55 +0900

    5.2 Data Flow

--- Home.py
+++ Home.py
@@ -1,10 +1,30 @@
 import streamlit as st
+from datetime import datetime
+
+today = datetime.today().strftime("%H:%M:%S")
+
+st.title(today)
 
 
-st.selectbox(
+model = st.selectbox(
     "Choose your model",
     (
         "GPT-3",
         "GPT-4",
     ),
 )
+
+if model == "GPT-3":
+    st.write("cheap")
+else:
+    st.write("not cheap")
+    name = st.text_input("What is your name?")
+    st.write(name)
+
+    value = st.slider(
+        "temperature",
+        min_value=0.1,
+        max_value=1.0,
+    )
+
+    st.write(value)
