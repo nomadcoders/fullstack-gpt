f3d9f42666fdd4318c740c852fcc0bbaecda96e7
니꼬 <nico@nomadcoders.co>
2023-11-10 20:16:18 +0900

    7.7 Grading Questions

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -264,8 +264,17 @@ if not docs:
     """
     )
 else:
-    start = st.button("Generate Quiz")
-
-    if start:
-        response = run_quiz_chain(docs, topic if topic else file.name)
-        st.write(response)
+    response = run_quiz_chain(docs, topic if topic else file.name)
+    with st.form("questions_form"):
+        for question in response["questions"]:
+            st.write(question["question"])
+            value = st.radio(
+                "Select an option.",
+                [answer["answer"] for answer in question["answers"]],
+                index=None,
+            )
+            if {"answer": value, "correct": True} in question["answers"]:
+                st.success("Correct!")
+            elif value is not None:
+                st.error("Wrong!")
+        button = st.form_submit_button()
