4e7935ce84633f3a771fb34d0a526d0ffa03c9c5
니꼬 <nico@nomadcoders.co>
2023-11-14 20:15:00 +0900

    8.6 Code Challenge

--- Home.py
+++ Home.py
@@ -16,7 +16,7 @@ Here are the apps I made:
 - [x] [DocumentGPT](/DocumentGPT)
 - [x] [PrivateGPT](/PrivateGPT)
 - [x] [QuizGPT](/QuizGPT)
-- [ ] [SiteGPT](/SiteGPT)
+- [x] [SiteGPT](/SiteGPT)
 - [ ] [MeetingGPT](/MeetingGPT)
 - [ ] [InvestorGPT](/InvestorGPT)
 """
