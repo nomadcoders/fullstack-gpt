5c4165d5bb536f152f80f2472a9892e34ff696bf
니꼬 <nico@nomadcoders.co>
2023-11-17 17:31:16 +0900

    11.3 GPT Action

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from pydantic import BaseModel, Field
 app = FastAPI(
     title="Nicolacus Maximus Quote Giver",
     description="Get a real quote said by Nicolacus Maximus himself.",
+    servers=[
+        {
+            "url": "https://rage-adapter-gtk-wooden.trycloudflare.com",
+        },
+    ],
 )
 
 
